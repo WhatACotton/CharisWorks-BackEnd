59e084c87c51d59e07f0ca33822d12e2630d3dfd
WhataACotton <whatacotton@gmail.com>
2023-08-01 20:24:04 +0900

    データベース修正

--- internal/models/models.go
+++ internal/models/models.go
@@ -14,7 +14,7 @@ type Item struct {
 type CustomerRequestPayload struct {
 	UID         string `json:"uid"`
 	Email       string `json:"contact"`
-	CreatedDate []uint8
+	CreatedDate string
 }
 type CustomerRegisterPayload struct {
 	Name        string `json:"Name"`
@@ -29,10 +29,10 @@ type Customer struct {
 	Email          string `json:"Contact"`
 	PhoneNumber    int    `json:"PhoneNumber"`
 	Register       bool
-	CreatedDate    []uint8
-	ModifiedDate   []uint8
-	RegisteredDate []uint8
-	LastLogInDate  []uint8
+	CreatedDate    string
+	ModifiedDate   string
+	RegisteredDate string
+	LastLogInDate  string
 }
 type LogInLog struct {
 	UID     string
@@ -51,8 +51,8 @@ type Transaction struct {
 	TransactionId   string `json:"transactionId"`
 	Count           string `json:"count"`
 	IsFinished      bool   `json:"isFinished"`
-	TransactionDate []uint8
-}
+	TransactionDate string
+}実装
 
 type Cart struct {
 	UID            string
@@ -69,5 +69,5 @@ type CartRequestPayload struct {
 
 type Session struct {
 	SessionId string
-	Date      []uint8
+	Date      string
 }
